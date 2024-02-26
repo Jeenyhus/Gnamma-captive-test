cdbf80a0287d0ee994c52a903b207a7b2b61e22a
Jeenyhus <mweembadabwitso@gmail.com>
2024-02-26 18:44:21 +0200

    added user model, inherits from the AbstractUser

--- captive_portal/models.py
+++ captive_portal/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 class UserProfile(models.Model):
     """
@@ -15,3 +15,19 @@ class UserProfile(models.Model):
         Returns a string representation of the user profile.
         """
         return self.username
+
+class User(AbstractUser):
+    """
+    Represents a user in the system.
+    """
+    email = models.EmailField(unique=True)
+    profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    username= models.CharField(max_length=20, unique=True)
+
+    class Meta:
+        # Ensure that the custom User model is used
+        swappable = 'AUTH_USER_MODEL'
+
+    # Specify unique related_name arguments for groups and user_permissions
+    groups = models.ManyToManyField(Group, related_name='custom_user_groups')
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions')
