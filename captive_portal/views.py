c09442320fbd98cdadb9889475fd39efebf08390
Jeenyhus <mweembadabwitso@gmail.com>
2024-02-26 18:42:24 +0200

    added network_id and organization_id, from api's

--- captive_portal/views.py
+++ captive_portal/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from .forms import SplashForm, UserProfileForm
-from .models import UserProfile
-from .meraki_utils import authenticate_with_meraki
-from django.contrib.auth.decorators import login_required
+from .forms import SplashForm, SignUpForm
+from .meraki_utils import get_organization_id, get_network_id, authenticate_meraki_user, grant_network_access
 
-@login_required
 def success(request):
     """
     Renders the success page.
@@ -22,34 +19,59 @@ def splash_page(request):
     if request.method == 'POST':
         form = SplashForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
             email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
 
-            user = authenticate_with_meraki(username, email)
-            if user:
-                login(request, user)
-                return redirect('captive_portal:success')
+            organization_id = get_organization_id()
+            network_name = 'Gnamma Tech AP1'  # Replace with your network name
+            network_id = get_network_id(organization_id, network_name)
+
+            if network_id:
+                if authenticate_meraki_user(network_id, email, password):
+                    user = authenticate(email=email, password=password)
+                    if user is not None:
+                        login(request, user)
+                        return redirect('captive_portal:success')
+                    else:
+                        form.add_error(None, 'User authentication failed.')
+                else:
+                    form.add_error(None, 'Meraki authentication failed. Please try again.')
             else:
-                form.add_error(None, 'Invalid credentials. Please try again.')
+                form.add_error(None, 'Failed to fetch network ID.')
     else:
         form = SplashForm()
 
-    # Store the URL the user was trying to access before being redirected to the splash page
     request.session['redirect_url'] = request.GET.get('next', '/')
-
     return render(request, 'captive_portal/splash.html', {'form': form})
 
-def sign_up(request):
+def sign_up_mk(request):
     """
     Renders the signup page and handles form submission.
     If the form is valid, saves the form data and redirects to the success page.
     """
     if request.method == 'POST':
-        form = UserProfileForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('captive_portal:success')
+            user = form.save(commit=False)
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            # Grant network access to the user
+            network_name = 'Gnamma Tech AP1'  # Replace with your network name
+            if not grant_network_access({'email': email, 'name': user.username, 'password': password}, network_name):
+                form.add_error(None, 'Failed to grant network access.')
+                return render(request, 'captive_portal/sign_up.html', {'form': form})
+
+            # Authenticate user and redirect to success page
+            user = authenticate(email=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('captive_portal:success')
+            else:
+                form.add_error(None, 'User authentication failed.')
     else:
-        form = UserProfileForm()
+        form = SignUpForm()
 
     return render(request, 'captive_portal/sign_up.html', {'form': form})
