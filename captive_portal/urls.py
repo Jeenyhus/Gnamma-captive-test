a5488d6cef8a00bec7b217fa4cec18687208a6c3
Jeenyhus <mweembadabwitso@gmail.com>
2024-02-26 18:40:25 +0200

    Fixed urls for signup

--- captive_portal/urls.py
+++ captive_portal/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'captive_portal'
 urlpatterns = [
     path('', views.splash_page, name='splash'),
-    path('sign_up/', views.sign_up, name='signup'),
+    path('sign_up_mk/', views.sign_up_mk, name='sign_up_mk'),
     path('success/', views.success, name='success'),
 ]
