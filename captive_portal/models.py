9aa1b9a9b44d80b741cbc1669634d2fa3d46b0a5
Jeenyhus <mweembadabwitso@gmail.com>
2024-02-27 10:12:38 +0200

    added User model

--- captive_portal/models.py
+++ captive_portal/models.py
@@ -1,33 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
-
-class UserProfile(models.Model):
-    """
-    Represents a user profile in the system.
-    """
-
-    username = models.CharField(max_length=100)
-    email = models.EmailField()
-    agreed_to_terms = models.BooleanField(default=False)
-
-    def __str__(self):
-        """
-        Returns a string representation of the user profile.
-        """
-        return self.username
+from django.contrib.auth import get_user_model
 
 class User(AbstractUser):
     """
     Represents a user in the system.
     """
     email = models.EmailField(unique=True)
-    profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
-    username= models.CharField(max_length=20, unique=True)
+    username = models.CharField(max_length=20, unique=True)
 
     class Meta:
         # Ensure that the custom User model is used
         swappable = 'AUTH_USER_MODEL'
 
-    # Specify unique related_name arguments for groups and user_permissions
+    def __str__(self):
+        """
+        Returns a string representation of the user.
+        """
+        return self.username
+    
     groups = models.ManyToManyField(Group, related_name='custom_user_groups')
     user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions')
+
+
+class UserProfile(models.Model):
+    """
+    Represents a user profile in the system.
+    """
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    network_id = models.CharField(max_length=100)
+    network_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        """
+        Returns a string representation of the user profile.
+        """
+        return self.user.username
