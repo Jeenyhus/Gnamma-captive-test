1ad3e82e06695254e682d25a0c58c8071dc5e466
Jeenyhus <mweembadabwitso@gmail.com>
2024-02-27 10:11:56 +0200

    modifed forms to work with the views

--- captive_portal/forms.py
+++ captive_portal/forms.py
@@ -1,39 +1,21 @@
 from django import forms
 from .models import UserProfile
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = '__all__'
 
-class SignUpForm(forms.ModelForm):
+class SignUpForm(forms.Form):
+    username = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput)
+    # Set default value for the role field to 'member'
+    role = forms.ChoiceField(choices=[('guest', 'Guest'), ('member', 'Member'), ('admin', 'Admin')], initial='member')
+    network_id = forms.CharField(max_length=100)
+    network_name = forms.CharField(max_length=100)
 
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password')  # Include necessary fields for signup
-
-class SplashForm(forms.Form):
-    email = forms.EmailField()  # Add email field
+class AuthenticationForm(forms.Form):
+    username = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput)
-
-    def clean(self):
-        cleaned_data = super().clean()
-        email = cleaned_data.get('email')
-        password = cleaned_data.get('password')
-
-        if not email or not password:
-            raise forms.ValidationError("Email and password are required.")
-
-    def authenticate_user(self):
-        """
-        Authenticates the user using provided email and password.
-        """
-        email = self.cleaned_data['email']
-        password = self.cleaned_data['password']
-        user = authenticate(email=email, password=password)
-        if user is None:
-            raise forms.ValidationError("Invalid email or password.")
-        return user
